e5fbca2ea911d7db23f371a0ba00a82552655af4
Jordan Evans <jordane@osuosl.org>
2014-07-07 11:49:25 -0700

    make template better

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -6,6 +6,8 @@ include Serverspec::Helper::DetectOS
 describe file('/etc/berkshelf/api-server/config.json') do
   it { should be_file }
   its(:content) { should match /github/ }
+  its(:content) { should match /faketoken/ }
+  its(:content) { should match /example.com/ }
 end
 
 describe command('python -m json.tool /etc/berkshelf/api-server/config.json') do
